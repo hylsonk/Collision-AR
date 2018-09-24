c03289c5e036a9a215ea2e8a48c5ab144e454abd
Hylson Kobayashi <hylsonk@gmail.com>
2018-09-23 22:44:36 -0300

    projeto de realidade aumentada com colisões

---
+++ Assets/BlueCube.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlueCube : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		gameObject.GetComponent<Renderer>().material.color = Color.blue;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Sphere.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sphere : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	private void OnTriggerEnter(Collider other) {
+		Debug.Log("Object Enter");
+		Color newColor;
+		newColor = other.GetComponent<Renderer>().material.color;
+		gameObject.GetComponent<Renderer>().material.color = newColor;
+	}
+
+	private void OnTriggerExit(Collider other) {
+		Debug.Log("Object Exit");
+		Color newColor = Color.white;
+		gameObject.GetComponent<Renderer>().material.color = newColor;
+	}
+
+	private void OnTriggerStay(Collider other) {
+		Debug.Log("Object Stay");
+	}
+}
